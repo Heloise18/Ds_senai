31d29b63a5204f4b4f7d7332bc3442516ac9b83c
Heloise18 <heloisezelma18@gmail.com>
2026-02-03 10:34:20 -0300

    continua

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -1,39 +1,46 @@
 
-show databases
+// show databases
 
-use ('jam')
+// use ('jam')
 
-const arrpeople = [
-    {
-        name: "Lays",
-        lastname: "Arceles",
-        salary: 0
+// const arrpeople = [
+//     {
+//         name: "Lays",
+//         lastname: "Arceles",
+//         salary: 0
 
-    },
-    {
-        name: "Fernanda",
-        lastname: "Fialho",
-        salary: 2.1
+//     },
+//     {
+//         name: "Fernanda",
+//         lastname: "Fialho",
+//         salary: 2.1
 
-    },
-    {
-        name: "Thais",
-        lastname: "Schitz",
-        salary: 2.1
+//     },
+//     {
+//         name: "Thais",
+//         lastname: "Schitz",
+//         salary: 2.1
 
-    },
-    {
-        name: "Rebeca",
-        lastname: "Ianz",
-        salary: 2.1
+//     },
+//     {
+//         name: "Rebeca",
+//         lastname: "Ianz",
+//         salary: 2.1
 
-    },
-    {
-        name: "Bruna",-
-        lastname: "Bonk",
-        salary: 2.1
+//     },
+//     {
+//         name: "Bruna",-
+//         lastname: "Bonk",
+//         salary: 2.1
 
-    }
-]
+//     }
+// ]
 
-db.people.insertMany(arrpeople);
+// db.people.insertMany(arrpeople);
+
+
+
+// use ('jam')
+// db.people.find({ salary: {$gt: 1}})
+// db.people.find({ salary: { $gte: 1}}, {name: 1, lastname: 1} )
+
