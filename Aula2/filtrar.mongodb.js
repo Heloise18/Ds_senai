82a59d7e41b1d79d05dff82d15c2060f5e4ef250
Heloise18 <heloisezelma18@gmail.com>
2026-02-04 09:13:06 -0300

    help

---
+++ Aula2/filtrar.mongodb.js
@@ -0,0 +1,14 @@
+use('atividade1')
+
+// db.contatos.find({nome: /^a/i})
+
+// db.contatos.find({telefone: /^9/})
+
+// db.contatos.find({email: /gmail.com/})
+
+// db.contatos.find({cidade: /São Paulo/})
+
+// db.contatos.find({dataCadastro: {$gt: new Date("2023-01-01")}})
+
+
+
