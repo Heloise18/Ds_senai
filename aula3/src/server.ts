fb16ec84e0dccc1f65daf59bd35b0f0f8fdc4680
Heloise18 <heloisezelma18@gmail.com>
2026-02-09 10:40:28 -0300

    routes

--- aula3/src/server.ts
+++ aula3/src/server.ts
@@ -3,36 +3,6 @@ import express, { response } from  'express';
 const port = 8011;
 const app = express()
 
-app.listen(port);
-
-const pessoas = [
-    {
-    name: "Helo",
-    Lastname: "Fachinello",
-    idade: "17.10"
-    },
-    {
-    name: "Cr",
-    Lastname: "Cardoso",
-    idade: "18"
-
-    }
-    
-]
-
-app.get('/', (req, res) => {
-    res.send({ response: "Api funegando! "})
-}); 
-
-app.get('/pessoa', (req, res) => {
-    // res.send({ people: pessoas})
-    res.status(200).send({pessoas})
-}); 
-
-
-// app.get('/direto', (req, res) => {
-//     res.send({ pessoa})
-// }); 
 
 app.listen(port, () => {
     console.log(` Servidor rodando na portra ${port}`);
