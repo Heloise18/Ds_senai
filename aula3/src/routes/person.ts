85252a9190c6c43934f73d003d09e5805281581d
Heloise18 <heloisezelma18@gmail.com>
2026-02-09 11:32:49 -0300

    post

--- aula3/src/routes/person.ts
+++ aula3/src/routes/person.ts
@@ -3,6 +3,13 @@ import express, { Request, Response, Router } from 'express';
 const router: Router = express.Router();
 const people: object[] = [];
 
-//....
+
+router
+    .post('/register', (req: Request , res: Response) => {
+        const { name, idade} = req.body
+        people.push({name, idade})
+        res.status(200).send({ message: `Usuario ${name} cadastrado com sucesso`  })
+
+    })
 
 export default router;
--- aula3/src/server.ts
+++ aula3/src/server.ts
@@ -1,8 +1,14 @@
 import express, { response } from  'express';
+import routes from './routes/routes.ts'
 
 const port = 8011;
 const app = express()
 
+routes (app)
+
+app.get('/', (req,res)=> {
+    res.status(200).send({response: "EBBAAA"})
+})
 
 app.listen(port, () => {
     console.log(` Servidor rodando na portra ${port}`);
