04f3320018e7a041cdfa36767c97e5d7f96adaba
Heloise18 <heloisezelma18@gmail.com>
2026-02-24 11:38:29 -0300

    delete, put, gets, post

--- Aula4/src/routes/alunos.ts
+++ Aula4/src/routes/alunos.ts
@@ -3,12 +3,97 @@ import express, { Request, Response, Router } from "express";
 const router: Router = express.Router();
 
 interface aluno {
-    id:number;
-    nome:string;
-    email:string;
-    tipo:"aluno";
-    ativo:boolean;
-    createdAT:Date;
+    id:number
+    nome:string
+    email:string
+    tipo:"aluno" | "professor" | "coordenador"
+    ativo:boolean
+    createdAT:Date
 }
 
 const aluno: aluno[] = [];
+
+router 
+    .post('/register', (req: Request, res:Response) =>{
+        const {id, nome, email, tipo, ativo} = req.body
+        
+        const createdAT = new Date()
+
+        if(ativo == null){
+            ativo == true;
+        }
+
+        if(nome == null || email == null){
+            return res.status(500).send({response: "Campos obrigatórios estão vazios!"});
+        }
+
+        const IdExists = aluno.find((i) => i.id == id)
+        
+        if(IdExists){
+            return res.status(500).send({ response: "Usuário já existe!" });
+        }
+        
+        const EmailExists = aluno.find((e) => e.email == email)
+        
+        if(EmailExists){
+            return res.status(500).send({ response: "Email já cadastrado!" });
+        }
+    
+        aluno.push({id, nome, email, tipo, ativo, createdAT})
+        return res.status(200).send({message:  `Usuario ${nome} cadastrado com sucesso` });
+    })
+
+    .get("/aluno", (req: Request, res: Response) => {
+       return res.status(200).send({aluno: aluno});
+    })
+
+    .get("/aluno/filtro/:tipo", (req: Request, res: Response) => {
+        const { tipo } = req.params;
+        const people = aluno.filter((a) => a.tipo == tipo );
+        return res.status(200).send({response: people});
+    })
+
+    .get("/aluno/:id", (req: Request, res: Response) => {
+        const { id } = req.params;
+        let convertedID = Number(id)
+        const AlunoExists = aluno.find((a) => a.id == convertedID);
+
+        if(!AlunoExists){
+            return res.status(404).send({ response: "Usuario não encontrado"})
+        }
+        return res.status(200).send({ aluno: AlunoExists})
+    })
+
+    .put("/atualizar/:id",  (req:Request, res:Response) => {
+        const { id } = req.params
+        const {  nome, email, tipo, ativo } = req.body
+        const Exists = aluno.find((a) => a.email == email )
+        
+        if(Exists){
+            return res.status(500).send({ response: "Email já cadastrado!" });
+        }
+
+
+
+        return res.status(200).send({response: `Atualizando usuario ${id} -> ${nome}`})
+    })       
+
+    
+    .delete("/deletar/:id", (req:Request, res:Response) => {
+        const { id } = req.params
+        let convertedID = Number(id)
+        const EXISTE = aluno.find((a) => a.id == convertedID )
+        
+        if(!EXISTE){
+            return res.status(404).send({ response: "Usuario não encontrado" });
+        }
+
+        return res.status(200).send({response: `Deletando usuario ${id}`})
+
+    })
+
+
+
+
+export default router;       
+
--- Aula4/src/server.ts
+++ Aula4/src/server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
+import routes from './routes/routes.ts';
 
 const app = express();
 const port = 8011;
+routes(app);
 
 app.listen(port, () => console.log(`Acesse: http://localhost:${port}/`));
