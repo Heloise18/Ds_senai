e8137184d6500e3f77791b14f519547234a4aba0
OsvaldoComCH <eduhrsilva@gmail.com>
2026-03-09 11:40:53 -0300

    front conected

--- Mongo Atividade copy/API/src/server.ts
+++ Mongo Atividade copy/API/src/server.ts
@@ -1,12 +1,21 @@
 import express from 'express';
 import routes from './routes/routes.ts';
 import connectDB from './database/database.ts';
-
+import cors from 'cors'
 
 const app = express();
 const port = 8011;
 
+app.use(cors({
+    origin: '*'
+}))
+
 connectDB();
 routes(app);
+
+
+app.get('/',(req, res )=> {
+   res.status(200).send({response: "Sucesso ao carregar a pagina"}) 
+})
 
 app.listen(port, () => console.log(`Acesse: http://localhost:${port}/`));
